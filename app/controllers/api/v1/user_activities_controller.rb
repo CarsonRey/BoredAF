e617a1a4c3c58c81cc169f4a15def6fd6642c1bc
CarsonRey <c.rey494@gmail.com>
2018-12-29 23:38:32 -0500

    userActivity  controller set up

--- app/controllers/api/v1/user_activities_controller.rb
+++ app/controllers/api/v1/user_activities_controller.rb
@@ -1,13 +1,19 @@
 class Api::V1::UserActivitiesController < ApplicationController
 
-
   def index
     @relationships = UserActivity.all
     render json: @relationships
   end
 
   def create
+    @user_activity = UserActivity.create(relationship_params)
+    if @user_activity.valid?
+      render json: @user_activity
+    end
+  end
 
+  def relationship_params
+    {activity_id: params["activity_id"], user_id: params["user_id"]}
   end
 
 end
